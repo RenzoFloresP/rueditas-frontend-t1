498dc84af02ed72e6fc8fbf368dc747c11e048e3
Renzo Flores <renzofpe@gmail.com>
2024-09-27 20:22:25 -0500

    Carga inicial de T1 rueditas!!

---
+++ src/main/java/pe/edu/cibertec/rueditas_frontend_t1/RueditasFrontendT1Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.rueditas_frontend_t1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RueditasFrontendT1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RueditasFrontendT1Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/rueditas_frontend_t1/RueditasFrontendT1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.rueditas_frontend_t1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RueditasFrontendT1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
